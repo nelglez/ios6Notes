d6c6afbb555afb8d21f14b1933584b0f8f40a2c0
Nelson Gonzalez <nelglezfl@gmail.com>
2019-04-24 13:26:06 -0400

    Added VC class

---
+++ Notes/NotesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotesViewController.swift
+//  Notes
+//
+//  Created by Nelson Gonzalez on 4/24/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
